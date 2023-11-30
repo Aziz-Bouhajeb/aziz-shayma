383598c5a076d6092cb8c10ebabd3dd0d101644a
Aziz-Bouhajeb <azizbouhajeb@gmail.com>
2023-11-30 01:35:01 +0100

    still trying

---
+++ html files/shop.js
@@ -0,0 +1,10 @@
+function makeProduct(name,price,availableColor,feature){
+    var product={}
+    product.name=name
+    product.price=price
+    product.availableColor=availableColor
+    product.feature=feature
+  return product
+}
+
+
